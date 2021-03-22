8172998877e345bdbc596f6e1330479f50039328
Jazaltron <jazaltron10@gmail.com>
2021-03-22 23:55:27 +0100

    My First TypeScript Commit

---
+++ LessonJournal.md
@@ -0,0 +1,13 @@
+# This File Simply Tracks All that I Haved learned So far
+
+console.log(character);
+
+const inputs = document.querySelectorAll('input');
+
+console.log(inputs);
+
+inputs.forEach(input => {
+    console.log(input);
+})
+
+/*You use tsc filename.ts -w for automatic recompilation anytime you save*/
--- sandbox.ts
+++ sandbox.ts
@@ -1,17 +1,22 @@
-var character = 'luigi';
-var age = 30;
-var Blackbelt = false;
-/*
+let character = 'luigi';
+let age = 30 ;
+let Blackbelt = false;
+
+/* 
 This gives an error anytime compiled
 character = 20;
 age = "yoshi"
 blackbelt = 43
 */
 /*Typescript infers the type based on the value we assign to our variables*/
+
 character = 'luigi';
 age = 96;
 Blackbelt = true;
-var circumference = function (diameter) {
+
+
+const circumference = (diameter:number) => {
     return diameter * Math.PI;
-};
-console.log(circumference(10.5));
+}
+
+console.log(circumference(78.7));
