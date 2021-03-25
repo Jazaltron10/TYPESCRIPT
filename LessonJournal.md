88d3316b5d8824dc4d901fadf7cbd03eeb8a8cea
Jazaltron <jazaltron10@gmail.com>
2021-03-25 09:56:45 +0100

    Made some changes to the ts file

--- LessonJournal.md
+++ LessonJournal.md
@@ -7,7 +7,15 @@ const inputs = document.querySelectorAll('input');
 console.log(inputs);
 
 inputs.forEach(input => {
-    console.log(input);
+console.log(input);
 })
 
-/*You use tsc filename.ts -w for automatic recompilation anytime you save*/
+In Typescript if you declare a variable to be a specific type then you
+can't change that type,
+
+also
+
+If you declare a property or an array to be of a specific type then you can't just change it down the line.
+
+once you have defined an object in typescript you then can't add additional properties to it.
+/_You use tsc filename.ts -w for automatic recompilation anytime you save_/
--- sandbox.ts
+++ sandbox.ts
@@ -20,3 +20,49 @@ const circumference = (diameter:number) => {
 }
 
 console.log(circumference(78.7));
+
+
+//arrays
+let names = ['Trevor', 'Michael', 'Franklin'];
+
+names.push('toad');
+// names.push(3);
+// names[0] = 3;
+
+
+let numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+numbers.push(25);
+//console.log(numbers);
+// numbers.push('shaun');
+// numbers[1]='shaun';
+
+
+let mixed = ['ken',4,'chun-li', 8, 9, true];
+
+mixed.push('sryu');
+mixed.push(10);
+
+mixed[0] = 3;
+
+
+//objects
+let ninja = {
+    name:'mario',
+    belt:'black',
+    age:30
+};
+
+ninja.age = 40;
+ninja.name = 'ryu';
+ninja.age = "30";
+
+ninja.skills = ['fighting', "sneaking"];
+
+
+ninja = {
+    name: "koshui",
+    belt: 'orange',
+    age: 40
+    skills: [] //gives error
+}
