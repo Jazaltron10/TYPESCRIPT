6f4104e236adfe6edfb00ae6683b2938b71484ad
Jazaltron <jazaltron10@gmail.com>
2021-04-15 22:43:52 +0100

    Little Changes To Sass and TS

--- sandbox.js
+++ sandbox.js
@@ -1,28 +1,18 @@
-var character = 'luigi';
+var character = 'mario';
 var age = 30;
-var Blackbelt = false;
-/*
-This gives an error anytime compiled
-character = 20;
-age = "yoshi"
-blackbelt = 43
-*/
-/*Typescript infers the type based on the value we assign to our variables*/
-character = 'luigi';
-age = 96;
-Blackbelt = true;
-var circumference = function (diameter) {
+var jump = 50;
+var circ = function (diameter) {
     return diameter * Math.PI;
 };
-console.log(circumference(78.7));
-//arrays
+console.log(circ(7.5));
+// arrays
 var names = ['Trevor', 'Michael', 'Franklin'];
 names.push('toad');
 // names.push(3);
 // names[0] = 3;
 var numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 numbers.push(25);
-//console.log(numbers);
+console.log(numbers);
 // numbers.push('shaun');
 // numbers[1]='shaun';
 var mixed = ['ken', 4, 'chun-li', 8, 9, true];
@@ -37,11 +27,11 @@ var ninja = {
 };
 ninja.age = 40;
 ninja.name = 'ryu';
-ninja.age = "30";
-ninja.skills = ['fighting', "sneaking"];
+//ninja.age = "30";
+//ninja.skills = ['fighting', "sneaking"];
 ninja = {
     name: "koshui",
     belt: 'orange',
-    age: 40,
-    skills: [] //gives error
+    age: 40
+    //skills: [] //gives error
 };
