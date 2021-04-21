482b48544fe14cb4a17b3efe17ffc6ae61292f08
Jazaltron <jazaltron10@gmail.com>
2021-04-21 22:54:43 +0100

    just the sandbox.ts file alone

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -3,12 +3,14 @@ let greet : Function;
 
 //greet = "john"
 
-greet =()=>{
+greet = () => {
     console.log("Hello world again");
 }
 
 /* 
-The Question mark makes calling the parameter optional when calling it in the function, likewise you can also give it a default value by equating it to that value. Note it is not advisable to use both the question mark and default value together you have to choose between either
+The Question mark makes calling the parameter optional when calling it in the function, likewise you can also give 
+it a default value by equating it to that value. Note it is not advisable to use both the question mark and 
+default value together you have to choose between either
 */
 
 /*
@@ -28,9 +30,30 @@ add(5 , 10, "20");
 /*We can explicitly define the return type of a function by adding a colon and specifying the return type after the parameter brackets
 Likewise a function in typescript returns void in typescript when we don't actually return anything
 but there's usually no need to do all this as typescript already infers the return type of any given function*/
+
 const minus  = (a:number, b:number):number => {
     return a + b;
 }
 
 let result = minus(10, 7);
 console.log (result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
